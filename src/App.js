69e3666cb2782dfe0ce1ead88c66b8d6e9a0b953
Walid <54804554+WalidTa@users.noreply.github.com>
2025-01-27 18:52:17 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>Welcome to Contactbeheer App</h1>
+      <p>Start managing your contacts!</p>
+    </div>
+  );
+}
+
+export default App;
